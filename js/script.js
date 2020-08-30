66785d4fa281d33cc85975d2d4d796f4726bb52c
Konstantin Kirillov <kirilloffkostya@gmail.com>
2020-08-30 21:32:14 +0300

    practice 2

--- js/script.js
+++ js/script.js
@@ -11,14 +11,39 @@ let personalMovieDB = {
     privat: false
 };
 
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+
 
 for (let i = 0; i < 2; i++) {
 
     let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
     let scoreFilm = +prompt('На сколько оцените его?', '');
 
+    if (lastFilm == '') {
+        --i;
+        continue;
+    }
+
+    if (lastFilm == null) {
+        --i;
+        continue;
+    }
+
+    if (lastFilm.length > 50)  {
+        --i;
+        continue;
+    }
+
     personalMovieDB.movies[lastFilm] = scoreFilm;
 
-};
+}
 
 console.log(personalMovieDB);
