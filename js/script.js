5b61ef98d5a15fadbb4c75719928fdea33bcb3ba
Konstantin Kirillov <kirilloffkostya@gmail.com>
2020-08-30 19:33:55 +0300

    lesson 12

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log(1);
+'use strict';
+
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+
+    let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+    let scoreFilm = +prompt('На сколько оцените его?', '');
+
+    personalMovieDB.movies[lastFilm] = scoreFilm;
+
+};
+
+console.log(personalMovieDB);
